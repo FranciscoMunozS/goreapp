fef0450f8597869751515d5e99e6734b31204699
Francisco <franciscomuniozs@gmail.com>
2017-11-16 09:14:28 -0300

    last update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,51 +1,8 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require jquery3
 //= require jquery_ujs
 //= require semantic-ui
 //= require calendar
 //= require custom_calendar
+//= require custom
 //= require_tree .
-
-$(document).ready(function() {
-  $('.ui.sidebar').sidebar('attach events', '#sidebutton');
-});
-
-$(document).ready(function() {
-  $('#due-date').calendar({
-    type: 'date',
-    monthFirst: false
-  });
-  $('#devolution_date').calendar({
-    type: 'date',
-    monthFirst: false
-  });
-  $('#income_date').calendar({
-    type: 'date',
-    monthFirst: false
-  });
-
-  $('.ui.dropdown').dropdown();
-
-  $('.ui.checkbox').checkbox();
-  
-});
-
-$(document).ready(function () {
-  setTimeout(function () {
-    $('.alert').fadeOut("slow", function () {
-      $(this).remove();
-    })
-  }, 2500);
-})
---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $('.ui.sidebar').sidebar('attach events', '#sidebutton');
+});
+
+$(document).ready(function () {
+  $('#due-date').calendar({
+    type: 'date',
+    monthFirst: false
+  });
+  $('#devolution_date').calendar({
+    type: 'date',
+    monthFirst: false
+  });
+  $('#income_date').calendar({
+    type: 'date',
+    monthFirst: false
+  });
+
+  $('.ui.dropdown').dropdown();
+
+  $('.ui.checkbox').checkbox();
+
+});
+
+$(document).ready(function () {
+  setTimeout(function () {
+    $('.alert').fadeOut("slow", function () {
+      $(this).remove();
+    })
+  }, 2500);
+})
